db21ca360578ac1375f37b9685816167259e2632
DaiDT <dtd300996@gmail.com>
2023-04-19 23:12:54 +0700

    update css

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" style={{
+      overflow: 'hidden',
+      height: '100%'
+    }}>
       <Head>
 
         <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;500;600;800&display=swap" rel="stylesheet" />
@@ -13,7 +16,10 @@ export default function Document() {
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       </Head>
-      <body>
+      <body style={{
+        overflow: 'auto',
+        height: '100%'
+      }}>
         <Main />
         <NextScript />
         <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
